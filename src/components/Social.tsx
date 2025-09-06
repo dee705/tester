58eb9a04846f0a2b74e3f6551ac09de84a27f47d
dee705 <essiralk@tuta.io>
2025-09-06 08:48:21 +0800

    Update Social.tsx

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,31 +1,41 @@
-import { Card, Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+
+// Ensure you have Font Awesome linked in your project.
+// If not, add this to your public/index.html or main layout:
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 const Social = () => {
   const socialPlatforms = [
     {
       name: "Instagram",
-      link: "https://www.instagram.com/klarissedguzman/"
+      link: "https://www.instagram.com/klarissedguzman/",
+      iconClass: "fab fa-instagram" // Updated Instagram icon
     },
     {
       name: "YouTube",
-      link: "https://www.youtube.com/@KLARISSEUpdates"
+      link: "https://www.youtube.com/@KLARISSEUpdates",
+      iconClass: "fab fa-youtube" // Updated YouTube icon
     },
     {
       name: "TikTok",
-      link: "https://www.tiktok.com/@klarissedguzman?lang=en&is_from_webapp=1&sender_device=mobile&sender_web_id=7531174735428011538"
+      link: "https://www.tiktok.com/@klarissedguzman?lang=en&is_from_webapp=1&sender_device=mobile&sender_web_id=7531174735428011538",
+      iconClass: "fab fa-tiktok" // Updated TikTok icon
     },
     {
       name: "Facebook",
-      link: "https://m.facebook.com/Klarisse06/"
+      link: "https://m.facebook.com/Klarisse06/",
+      iconClass: "fab fa-facebook-f" // Updated Facebook icon
     },
     {
       name: "Twitter",
-      link: "https://x.com/Klarissedguzman?t=ZFXgmlgJTyf0daUNbcUguA&s=09"
+      link: "https://x.com/Klarissedguzman?t=ZFXgmlgJTyf0daUNbcUguA&s=09",
+      iconClass: "fab fa-x-twitter" // Updated Twitter (X) icon
     },
     {
       name: "Spotify",
-      link: "https://open.spotify.com/artist/7r59WFPJdYBQmnIQB4DX7K?si=3D0wFIiMSieyWycCGO7bTw"
+      link: "https://open.spotify.com/artist/7r59WFPJdYBQmnIQB4DX7K?si=3D0wFIiMSieyWycCGO7bTw",
+      iconClass: "fab fa-spotify" // Updated Spotify icon
     }
   ];
 
@@ -43,8 +53,10 @@ const Social = () => {
                 key={index}
                 variant="outline"
                 onClick={() => window.open(platform.link, '_blank')}
-                className="w-full sm:w-auto"
+                className="w-full sm:w-auto flex items-center gap-2" // Added flex and gap for icon alignment
               >
+                {/* Render the Font Awesome icon */}
+                <i className={platform.iconClass}></i> 
                 {platform.name}
               </Button>
             ))}
