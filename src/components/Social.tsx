b386c301042d05dc2b804caf4159c7e0059d8718
dee705 <essiralk@tuta.io>
2025-09-06 08:51:39 +0800

    Update Social.tsx

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,41 +1,43 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
-// Ensure you have Font Awesome linked in your project.
-// If not, add this to your public/index.html or main layout:
-// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
 const Social = () => {
   const socialPlatforms = [
     {
       name: "Instagram",
       link: "https://www.instagram.com/klarissedguzman/",
-      iconClass: "fab fa-instagram" // Updated Instagram icon
+      iconClass: "fab fa-instagram",
+      iconColorClass: "bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500" // A custom gradient for Instagram
     },
     {
       name: "YouTube",
       link: "https://www.youtube.com/@KLARISSEUpdates",
-      iconClass: "fab fa-youtube" // Updated YouTube icon
+      iconClass: "fab fa-youtube",
+      iconColorClass: "bg-gradient-to-r from-red-500 to-red-600"
     },
     {
       name: "TikTok",
       link: "https://www.tiktok.com/@klarissedguzman?lang=en&is_from_webapp=1&sender_device=mobile&sender_web_id=7531174735428011538",
-      iconClass: "fab fa-tiktok" // Updated TikTok icon
+      iconClass: "fab fa-tiktok",
+      iconColorClass: "bg-gradient-to-r from-black via-pink-600 to-black"
     },
     {
       name: "Facebook",
       link: "https://m.facebook.com/Klarisse06/",
-      iconClass: "fab fa-facebook-f" // Updated Facebook icon
+      iconClass: "fab fa-facebook-f",
+      iconColorClass: "bg-gradient-to-r from-blue-600 to-blue-700"
     },
     {
       name: "Twitter",
       link: "https://x.com/Klarissedguzman?t=ZFXgmlgJTyf0daUNbcUguA&s=09",
-      iconClass: "fab fa-x-twitter" // Updated Twitter (X) icon
+      iconClass: "fab fa-x-twitter",
+      iconColorClass: "text-black" // Twitter (X) logo is solid black
     },
     {
       name: "Spotify",
       link: "https://open.spotify.com/artist/7r59WFPJdYBQmnIQB4DX7K?si=3D0wFIiMSieyWycCGO7bTw",
-      iconClass: "fab fa-spotify" // Updated Spotify icon
+      iconClass: "fab fa-spotify",
+      iconColorClass: "bg-gradient-to-r from-green-500 to-green-600"
     }
   ];
 
@@ -53,10 +55,10 @@ const Social = () => {
                 key={index}
                 variant="outline"
                 onClick={() => window.open(platform.link, '_blank')}
-                className="w-full sm:w-auto flex items-center gap-2" // Added flex and gap for icon alignment
+                className="w-full sm:w-auto flex items-center gap-2"
               >
-                {/* Render the Font Awesome icon */}
-                <i className={platform.iconClass}></i> 
+                {/* Rendering the icon with gradient colors */}
+                <i className={`${platform.iconClass} ${platform.iconColorClass} bg-clip-text text-transparent`}></i>
                 {platform.name}
               </Button>
             ))}
