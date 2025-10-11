4b984e3114a8b30e466c80513798ff08630966e0
dee705 <essiralk@tuta.io>
2025-10-11 23:34:52 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Music from "@/components/Music";
-//import Concert from "@/components/Concert"; //
+// import Concert from "@/components/Concert"; // Removed import
 import PhotoCarousel from "@/components/PhotoCarousel";
 import Social from "@/components/Social";
 import Fanpage from "@/components/Fanpage";
@@ -10,20 +10,20 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      <Hero />
-      <About />
-      <Music />
-      { //<Concert /> // }
-      <PhotoCarousel />
-      <Social />
-      <Fanpage />
-      <Contact />
-      <Footer />
-    </div>
-  );
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Music />
+      {/* Removed <Concert /> entirely */} 
+      <PhotoCarousel />
+      <Social />
+      <Fanpage />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default Index;
